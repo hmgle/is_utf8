ea6e0389a8bce785e4e2796e3a59babf741247f6
mingang.he <dustgle@gmail.com>
2013-03-27 20:41:47 +0800

    added detect_file_encoding()

--- is_utf8.c
+++ is_utf8.c
@@ -46,3 +46,51 @@ int is_utf8(unsigned char *str, size_t len)
     return 0;
 }
 
+int
+detect_file_encoding(FILE *text_fp)
+{
+	int ret;
+	fpos_t pos;
+	uint8_t buf[MAX_LINE];
+	size_t len;
+	enum file_encoding_type encoding_type;
+
+	ret = fgetpos(text_fp, &pos);
+	assert(!ret);
+
+	rewind(text_fp);
+	if (fgets((char *)buf, sizeof(buf), text_fp) == NULL)
+		return -1;
+	len = strlen((char *)buf);
+	if (len >= 3 && buf[0] == 0xef && buf[1] == 0xbb && buf[2] == 0xbf) {
+		encoding_type = UTF8_WITH_BOM;
+		goto end;
+	}
+	else {
+		ret = is_utf8(buf, len);
+		if (ret > 0) {
+			encoding_type = GBK;
+			goto end;
+		} else if (ret == -1) {
+			encoding_type = UTF8_NO_BOM;
+			goto end;
+		}
+	}
+
+	while (fgets((char *)buf, sizeof(buf), text_fp)) {
+		len = strlen((char *)buf);
+		ret = is_utf8(buf, len);
+		if (ret > 0) {
+			encoding_type = GBK;
+			goto end;
+		} else if (ret == -1) {
+			encoding_type = UTF8_NO_BOM;
+			goto end;
+		}
+	}
+	encoding_type = UTF8_NO_BOM;
+end:
+	ret = fsetpos(text_fp, &pos);
+	assert(!ret);
+	return encoding_type;
+}
--- is_utf8.h
+++ is_utf8.h
@@ -2,7 +2,22 @@
 #define _IS_UTF8_H
 
 #include <stdlib.h>
+#include <stdint.h>
+#include <stdio.h>
+#include <string.h>
+#include <assert.h>
+
+#ifndef MAX_LINE
+#define MAX_LINE	1024
+#endif
+
+enum file_encoding_type {
+	UTF8_NO_BOM,
+	UTF8_WITH_BOM,
+	GBK
+};
 
 int is_utf8(unsigned char *str, size_t len);
+int detect_file_encoding(FILE *text_fp);
 
 #endif /* _IS_UTF8_H */
